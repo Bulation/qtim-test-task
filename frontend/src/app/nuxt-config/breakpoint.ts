08aa649a20bb79f293b67b6a4ecff02fc083b970
Bulation <insanbulat@gmail.com>
2025-10-26 18:04:32 +0300

    feat: добавит конфиг для nuxt-viewport

---
+++ frontend/src/app/nuxt-config/breakpoint.ts
@@ -0,0 +1,17 @@
+const BREAKPOINTS = {
+  'xxl': 1920,
+  'xl': 1680,
+  'lg': 1280,
+  'md': 768,
+  'sm': 420,
+  'xs': 320,
+};
+
+export default {
+  breakpoints: BREAKPOINTS,
+  defaultBreakpoints: {
+    desktop: 'lg',
+    mobile: 'sm',
+    tablet: 'md',
+  },
+};
