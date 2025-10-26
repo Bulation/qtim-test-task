ffa93550558735e0dd3a598da35c3392b47872ba
Bulation <insanbulat@gmail.com>
2025-10-26 19:14:22 +0300

    fix: исправит ошибку еслинта

--- frontend/src/app/nuxt-config/breakpoint.ts
+++ frontend/src/app/nuxt-config/breakpoint.ts
@@ -1,10 +1,10 @@
 const BREAKPOINTS = {
-  'xxl': 1920,
-  'xl': 1680,
-  'lg': 1280,
-  'md': 768,
-  'sm': 420,
-  'xs': 320,
+  xxl: 1920,
+  xl: 1680,
+  lg: 1280,
+  md: 768,
+  sm: 420,
+  xs: 320,
 };
 
 export default {
