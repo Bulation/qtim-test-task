42323c773321aa4e8f451518b2c64bc4e27d2120
Bulation <insanbulat@gmail.com>
2025-10-26 19:18:43 +0300

    refactor: переработает страницу ошибки

--- frontend/src/error.vue
+++ frontend/src/error.vue
@@ -18,43 +18,38 @@ useSeoMeta({
 </script>
 
 <template>
-  <div class="error-page">
-    <div class="status-code-wrap">
+  <NuxtLayout>
+    <div class="error-page">
       <h2 class="error-code">
         {{ error.statusCode }}
       </h2>
+      <div class="error-container">
+        <h3 class="error-message">
+          {{ name }}
+        </h3>
+        <p class="desc">
+          {{ message }}
+        </p>
+        <button class="link" @click="clearError({ redirect: '/' })">
+          Перейти на главную
+        </button>
+      </div>
     </div>
-    <div class="app-container error-container">
-      <h3 class="error-message">
-        {{ name }}
-      </h3>
-      <p class="desc">{{ message }}</p>
-      <button @click="clearError({ redirect: '/' })" class="default-button link">
-        Перейти на главную
-      </button>
-    </div>
-  </div>
+  </NuxtLayout>
 </template>
 
 <style scoped lang="scss">
 .error-page {
   display: grid;
   height: 100%;
+  padding: 100px 10px;
   grid-template-columns: 100%;
   grid-template-rows: auto auto 1fr;
 
-  .status-code-wrap {
-    position: relative;
-    margin-bottom: 50px;
-  }
-
   .error-code {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    font-size: 220px;
+    font-size: 150px;
     font-weight: 700;
-    transform: translate(-50%, -50%);
+    text-align: center;
   }
 
   .error-container {
@@ -74,10 +69,14 @@ useSeoMeta({
     margin-bottom: 21px;
     font-size: 14px;
     font-weight: 400;
+    text-align: center;
   }
 
   .link {
     display: block;
+    padding: 8px;
+    border: 1px solid $black;
+    border-radius: 8px;
     margin: 0 auto;
   }
 }
