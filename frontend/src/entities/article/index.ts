081d29ebfd5f488260ad7ee1720e4556d4bc1a69
Bulation <insanbulat@gmail.com>
2025-10-26 19:24:29 +0300

    feat: реализует экспорт стора и компонентов статьи через публичный апи

---
+++ frontend/src/entities/article/index.ts
@@ -0,0 +1,4 @@
+export { useArticleStore } from './model/article-store';
+export { default as ArticleCard } from './ui/ArticleCard.vue';
+export { default as DetailArticle } from './ui/DetailArticle.vue';
+export { ARTICLES_LIMIT } from './model/constants';
