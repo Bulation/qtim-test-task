4d955e321e7ea4f89b203961c5faeec1c22f3e2c
Bulation <insanbulat@gmail.com>
2025-10-26 19:21:57 +0300

    feat: реализует карточку статьи

---
+++ frontend/src/entities/article/ui/ArticleCard.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import type { Article } from '../model/types';
+
+interface ArticleCardProps {
+  article: Article,
+}
+defineProps<ArticleCardProps>();
+</script>
+
+<template>
+  <article class="article" itemscope itemtype="https://schema.org/Article">
+    <NuxtLink :to="`/${article.id}`" itemprop="url">
+      <div class="image-wrapper">
+        <NuxtImg
+          :src="article.image"
+          :alt="`Изображение статьи ${article.title}`"
+          sizes="280px"
+          itemprop="image"
+          class="img"
+          placeholder="/placeholder.svg"
+        />
+      </div>
+      <p class="text" itemprop="description">
+        {{ article.preview }}
+      </p>
+      <div class="read-more">
+        Read more
+      </div>
+    </NuxtLink>
+  </article>
+</template>
+
+<style lang="scss" scoped>
+.article {
+  font-size: 20px;
+  line-height: 120%;
+  transition: transform 0.3s;
+
+  &:hover {
+    transform: translateY(-20px);
+
+    .read-more {
+      opacity: 1;
+    }
+  }
+
+  .image-wrapper {
+    display: flex;
+    width: 280px;
+    height: 280px;
+    margin-bottom: 24px;
+
+    .img {
+      width: 100%;
+      object-fit: cover;
+    }
+  }
+
+  .text {
+    color: $black;
+  }
+
+  .read-more {
+    margin-top: 12px;
+    color: $purple;
+    opacity: 0;
+  }
+}
+</style>
